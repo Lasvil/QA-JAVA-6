2ee2e59f4d9b55946de9f7deaa54e97fb74abbfd
ArtyomKiyashko <lounershoargent@gmail.com>
2023-06-22 21:26:01 +0300

    Push homework

---
+++ src/test/java/RestMonthTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RestMonthTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/restMonth.csv")
+    public void calculateRest(int income, int expenses, int threshold) {
+        RestMonth service = new RestMonth();
+        int rest = service.calculate(income, expenses, threshold);
+        System.out.println("Месяцев отдыха в году - " + rest);
+
+    }
+}
